ef464298e766d49b0561022a952a061a3456d8b7
aminulislam67 <98544001+aminulislam67@users.noreply.github.com>
2023-06-04 11:19:17 +0600

    9

--- display.js
+++ display.js
@@ -1,179 +1,218 @@
 var storedData = JSON.parse(localStorage.getItem("studentData")) || [];
+    var currentPage = 1;
+    var pageSize = 2;
+    var totalPages = Math.ceil(storedData.length / pageSize);
+    var filteredData = [];
 
-// Function to filter the students based on search input
-function filterStudents() {
-  var nameInput = document.getElementById("nameInput").value.toLowerCase();
-  var idInput = document.getElementById("idInput").value.toLowerCase();
+    function paginate() {
+      // Clear the table body
+      var dataBody = document.getElementById("dataBody");
+      dataBody.innerHTML = "";
 
-  // Clear the table body
-  var dataBody = document.getElementById("dataBody");
-  dataBody.innerHTML = "";
+      var startIndex = (currentPage - 1) * pageSize;
+      var endIndex = startIndex + pageSize;
 
-  // Filter the student data based on search input
-  var filteredData = storedData.filter(function(student) {
-    var nameMatch = student.name.toLowerCase().includes(nameInput);
-    var idMatch = student.studentId.toLowerCase().includes(idInput);
+      // Get the current page data
+      var pageData = filteredData.slice(startIndex, endIndex);
 
-    if (nameInput && idInput) {
-      return nameMatch && idMatch;
-    } else if (nameInput) {
-      return nameMatch;
-    } else if (idInput) {
-      return idMatch;
-    } else {
-      return true; // No search input provided
+      // Iterate over each student data object in the current page and create table rows
+      pageData.forEach(function(student, index) {
+        var row = document.createElement("tr");
+
+        var checkboxCell = document.createElement("td");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "studentCheckbox";
+        checkbox.value = startIndex + index;
+        checkboxCell.appendChild(checkbox);
+        row.appendChild(checkboxCell);
+
+        var serialNumberCell = document.createElement("td");
+        serialNumberCell.textContent = startIndex + index + 1;
+        row.appendChild(serialNumberCell);
+
+        var nameCell = document.createElement("td");
+        nameCell.textContent = student.name;
+        row.appendChild(nameCell);
+
+        var studentIdCell = document.createElement("td");
+        studentIdCell.textContent = student.studentId;
+        row.appendChild(studentIdCell);
+
+        var emailCell = document.createElement("td");
+        emailCell.textContent = student.email;
+        row.appendChild(emailCell);
+
+        var ageCell = document.createElement("td");
+        ageCell.textContent = student.age;
+        row.appendChild(ageCell);
+
+        var sessionCell = document.createElement("td");
+        sessionCell.textContent = student.session;
+        row.appendChild(sessionCell);
+
+        var actionsCell = document.createElement("td");
+        var editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.classList.add("btn", "btn-primary");
+        editButton.addEventListener("click", function() {
+          editStudent(startIndex + index);
+        });
+        actionsCell.appendChild(editButton);
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("btn", "btn-danger");
+        deleteButton.addEventListener("click", function() {
+          deleteStudent(startIndex + index);
+        });
+        actionsCell.appendChild(deleteButton);
+
+        row.appendChild(actionsCell);
+
+        dataBody.appendChild(row);
+      });
+
+      // Generate pagination links
+      var paginationDiv = document.getElementById("pagination");
+      paginationDiv.innerHTML = "";
+
+      var previousButton = document.createElement("button");
+      previousButton.textContent = "Previous";
+      previousButton.classList.add("btn", "btn-primary");
+      previousButton.disabled = currentPage === 1;
+      previousButton.addEventListener("click", function() {
+        currentPage--;
+        paginate();
+      });
+      paginationDiv.appendChild(previousButton);
+
+      var pageNumbers = document.createElement("div");
+      pageNumbers.textContent = "Pages: ";
+
+      var firstPageLink = document.createElement("a");
+      firstPageLink.href = "#";
+      firstPageLink.textContent = "1";
+      firstPageLink.dataset.page = 1;
+      firstPageLink.addEventListener("click", function(event) {
+        currentPage = parseInt(event.target.dataset.page);
+        paginate();
+      });
+      pageNumbers.appendChild(firstPageLink);
+
+      if (currentPage > 3) {
+        var ellipsisStart = document.createElement("span");
+        ellipsisStart.textContent = "...";
+        pageNumbers.appendChild(ellipsisStart);
+      }
+
+      var startPage = currentPage > 2 ? currentPage - 1 : 2;
+      var endPage = currentPage < totalPages - 1 ? currentPage + 1 : totalPages - 1;
+      for (var i = startPage; i <= endPage; i++) {
+        var pageLink = document.createElement("a");
+        pageLink.href = "#";
+        pageLink.textContent = i;
+        pageLink.dataset.page = i;
+        pageLink.classList.add("pagination-link");
+        pageLink.addEventListener("click", function(event) {
+          currentPage = parseInt(event.target.dataset.page);
+          paginate();
+        });
+        pageNumbers.appendChild(pageLink);
+      }
+
+      if (currentPage < totalPages - 2) {
+        var ellipsisEnd = document.createElement("span");
+        ellipsisEnd.textContent = "...";
+        pageNumbers.appendChild(ellipsisEnd);
+      }
+
+      if (totalPages > 1) {
+        var lastPageLink = document.createElement("a");
+        lastPageLink.href = "#";
+        lastPageLink.textContent = totalPages;
+        lastPageLink.dataset.page = totalPages;
+        lastPageLink.classList.add("pagination-link");
+        lastPageLink.addEventListener("click", function(event) {
+          currentPage = parseInt(event.target.dataset.page);
+          paginate();
+        });
+        pageNumbers.appendChild(lastPageLink);
+      }
+
+      paginationDiv.appendChild(pageNumbers);
+
+      var nextButton = document.createElement("button");
+      nextButton.textContent = "Next";
+      nextButton.classList.add("btn", "btn-primary");
+      nextButton.disabled = currentPage === totalPages;
+      nextButton.addEventListener("click", function() {
+        currentPage++;
+        paginate();
+      });
+      paginationDiv.appendChild(nextButton);
     }
-  });
 
-  // Iterate over each filtered student data object and create table rows
-  filteredData.forEach(function(student, index) {
-    var row = document.createElement("tr");
-    
-    var checkboxCell = document.createElement("td");
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = "studentCheckbox";
-    checkbox.value = index;
-    checkboxCell.appendChild(checkbox);
-    row.appendChild(checkboxCell);
+    function filterStudents() {
+      var searchInput = document.getElementById("searchInput").value.toLowerCase();
+      filteredData = [];
 
-    var serialNumberCell = document.createElement("td");
-    serialNumberCell.textContent = index + 1;
-    row.appendChild(serialNumberCell);
+      if (searchInput) {
+        filteredData = storedData.filter(function(student) {
+          var fullName = student.name.toLowerCase();
+          var studentId = student.studentId.toLowerCase();
+          return fullName === searchInput || studentId === searchInput;
+        });
+      } else {
+        filteredData = storedData;
+      }
 
-    var nameCell = document.createElement("td");
-    nameCell.textContent = student.name;
-    row.appendChild(nameCell);
+      currentPage = 1;
+      totalPages = Math.ceil(filteredData.length / pageSize);
+      paginate();
+    }
 
-    var studentIdCell = document.createElement("td");
-    studentIdCell.textContent = student.studentId;
-    row.appendChild(studentIdCell);
+    function deleteSelectedStudents() {
+      var checkboxes = document.getElementsByName("studentCheckbox");
+      var selectedIndexes = [];
 
-    var emailCell = document.createElement("td");
-    emailCell.textContent = student.email;
-    row.appendChild(emailCell);
+      checkboxes.forEach(function(checkbox, index) {
+        if (checkbox.checked) {
+          selectedIndexes.push(parseInt(checkbox.value));
+        }
+      });
 
-    var ageCell = document.createElement("td");
-    ageCell.textContent = student.age;
-    row.appendChild(ageCell);
+      selectedIndexes.sort(function(a, b) {
+        return b - a;
+      });
 
-    var sessionCell = document.createElement("td");
-    sessionCell.textContent = student.session;
-    row.appendChild(sessionCell);
+      selectedIndexes.forEach(function(index) {
+        storedData.splice(index, 1);
+      });
 
-    var actionsCell = document.createElement("td");
-    var editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.style.backgroundColor="gold";
-        editButton.style.color="black";
-        // editButton.style.marginRight="15px";
-        editButton.style.width="100px";
-        editButton.style.border="3px solid black";
-        editButton.style.fontSize="1em";
-        editButton.style.fontWeight="bold";
-        editButton.style.marginRight="5px";
-    // Add your edit button styles here
-    editButton.addEventListener("click", function() {
-      editStudent(index);
+      localStorage.setItem("studentData", JSON.stringify(storedData));
+
+      filterStudents();
+    }
+
+    function editStudent(index) {
+     
+       window.location.href = "edit.html?index=" + index;
+    }
+
+    function deleteStudent(index) {
+      storedData.splice(index, 1);
+      localStorage.setItem("studentData", JSON.stringify(storedData));
+      filterStudents();
+    }
+
+    document.getElementById("searchButton").addEventListener("click", filterStudents);
+    document.getElementById("selectAllCheckbox").addEventListener("change", function() {
+      var checkboxes = document.getElementsByName("studentCheckbox");
+      checkboxes.forEach(function(checkbox) {
+        checkbox.checked = event.target.checked;
+      });
     });
-    actionsCell.appendChild(editButton);
+    document.getElementById("deleteSelectedButton").addEventListener("click", deleteSelectedStudents);
 
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.style.backgroundColor="tomato";
-        deleteButton.style.color="black";
-        // deleteButton.style.marginRight="15px";
-        deleteButton.style.width="100px";
-        deleteButton.style.border="3px solid black";
-        deleteButton.style.fontSize="1em";
-       
-  
-        deleteButton.style.fontWeight="bold";
-    // Add your delete button styles here
-    deleteButton.addEventListener("click", function() {
-      deleteStudent(index);
-    });
-    actionsCell.appendChild(deleteButton);
-
-    row.appendChild(actionsCell);
-
-    dataBody.appendChild(row);
-  });
-}
-
-// Function to delete a student
-function deleteStudent(index) {
-  // Retrieve student data from localStorage
-  var storedData = JSON.parse(localStorage.getItem("studentData")) || [];
-
-  // Remove the student data object at the specified index
-  storedData.splice(index, 1);
-
-  // Update the localStorage with the modified student data
-  localStorage.setItem("studentData", JSON.stringify(storedData));
-
-  // Reload the page
-  window.location.reload();
-}
-
-// Function to delete multiple students
-function deleteSelected() {
-  var checkboxes = document.querySelectorAll('input[name="studentCheckbox"]:checked');
-  var indexesToDelete = Array.from(checkboxes).map(function(checkbox) {
-    return parseInt(checkbox.value);
-  });
-
-  // Retrieve student data from localStorage
-  var storedData = JSON.parse(localStorage.getItem("studentData")) || [];
-
-  // Remove the selected student data objects
-  indexesToDelete.sort(function(a, b) {
-    return b - a;
-  }).forEach(function(index) {
-    storedData.splice(index, 1);
-  });
-
-  // Update the localStorage with the modified student data
-  localStorage.setItem("studentData", JSON.stringify(storedData));
-
-  // Reload the page
-  window.location.reload();
-}
-
-// Function to edit a student
-function editStudent(index) {
-  // Redirect to the edit page with the index as a URL parameter
-  window.location.href = "edit.html?index=" + index;
-}
-
-// Call the filterStudents function initially to display all students
-filterStudents();
-
-// Add event listeners to the search inputs for filtering students
-document.getElementById("searchByNameButton").addEventListener("click", function() {
-  filterStudents();
-});
-
-document.getElementById("searchByIdButton").addEventListener("click", function() {
-  filterStudents();
-});
-
-// Add a click event listener to the "Delete Selected" button
-var deleteSelectedButton = document.getElementById("deleteSelectedButton");
-deleteSelectedButton.style.backgroundColor="lightgreen";
-deleteSelectedButton.style.color="black";
-        // deleteButton.style.marginRight="15px";
-        deleteSelectedButton.style.width="100px";
-        deleteSelectedButton.style.border="3px solid black";
-        deleteSelectedButton.style.fontSize="1em";
-        deleteSelectedButton.style.fontWeight="bold";
-deleteSelectedButton.addEventListener("click", deleteSelected);
-
-// Add event listener for the select all checkbox
-var selectAllCheckbox = document.getElementById("selectAllCheckbox");
-selectAllCheckbox.addEventListener("change", function() {
-  var checkboxes = document.querySelectorAll('input[name="studentCheckbox"]');
-  checkboxes.forEach(function(checkbox) {
-    checkbox.checked = selectAllCheckbox.checked;
-  });
-});
+    filterStudents()
